53e8d55d7d41cb6d2575e6d252c05b4b407cb20f
timkwali <timkwalit@accessbankplc.com>
2023-12-11 14:14:57 +0100

    add Article entity, model and repository

---
+++ lib/features/daily_news/data/repository/article_repository_impl.dart
@@ -0,0 +1,12 @@
+import 'package:news_app_clean_architecture/core/resources/data_state.dart';
+import 'package:news_app_clean_architecture/features/daily_news/data/models/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/entities/article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
+
+class ArticleRepositoryImpl implements ArticleRepository{
+  @override
+  Future<DataState<List<ArticleModel>>> getNewsArticles() {
+    throw UnimplementedError();
+  }
+
+}
