5601a695fa288c63c16a4f6d2d3f3c8b89a93cad
timkwali <timkwalit@accessbankplc.com>
2023-12-14 08:36:39 +0100

    add usecases to injection_container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -5,7 +5,11 @@ import 'package:news_app_clean_architecture/features/daily_news/data/data_source
 import 'package:news_app_clean_architecture/features/daily_news/data/repository/article_repository_impl.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_saved_article.dart';
+import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/save_article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+
+import 'features/daily_news/domain/usecases/remove_article.dart';
 
 final sl = GetIt.instance;
 
@@ -25,6 +29,12 @@ Future<void> initializeDependencies() async {
   //usecases
   sl.registerSingleton<GetArticleUseCase>(GetArticleUseCase(sl()));
   
+  sl.registerSingleton<GetSavedArticleUseCase>(GetSavedArticleUseCase(sl()));
+  
+  sl.registerSingleton<SaveArticleUseCase>(SaveArticleUseCase(sl()));
+
+  sl.registerSingleton<RemoveArticleUseCase>(RemoveArticleUseCase(sl()));
+
   //blocs
   sl.registerFactory<RemoteArticlesBloc>(() => RemoteArticlesBloc(sl()));
 }
